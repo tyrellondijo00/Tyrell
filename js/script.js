e0290d1ef097ed1fc14fee9d64e55848d22e9c57
Tyrell Ondijo <tyrellondijo@yahoo.com>
2022-04-24 20:09:46 +0300

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$('.animated').appear(function(){
+    var el = $(this);
+    var anim = el.data('animation');
+    var animDelay = el.data('delay');
+    if (animDelay) {
+
+        setTimeout(function(){
+            el.addClass( anim + " in" );
+            el.removeClass('out');
+        }, animDelay);
+
+    }
+
+    else {
+        el.addClass( anim + " in" );
+        el.removeClass('out');
+    }    
+},{accY: -150});  
